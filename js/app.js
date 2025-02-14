eeff80a834916e06248e618f3bfaadc7b2228f2b
OCTO <chobotnicecz@gmail.com>
2025-02-14 10:41:44 +0100

    refactor click load  event

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 const root = document.getElementById("root");
-let itemUrl = "";
-let currentType = "";
 
 function createButton(text) {
   const button = document.createElement("button");
@@ -43,30 +41,6 @@ function clearContent() {
   list.innerHTML = "";
 }
 
-function responseApi(type) {
-  if (type) {
-    itemUrl = `https://swapi.dev/api/${type}`;
-    currentType = type; 
-    clearContent();
-  }
-
-  const loadBtn = document.querySelector(".load-btn");
-
-  fetch(itemUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      itemUrl = data.next;
-
-      if (data.next) {
-        loadBtn.classList.add("show");
-      } else {
-        loadBtn.classList.remove("show");
-      }
-      showItems(data.results, currentType);
-      window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
-    });
-}
-
 function showItems(data, type) {
   const parent = document.querySelector(".items-list");
 
@@ -76,6 +50,47 @@ function showItems(data, type) {
     element.addEventListener("click", () => showPopup(item, type));
     parent.appendChild(element);
   });
+}
+
+function createDetailElement(label, value) {
+  const detailElement = document.createElement("h5");
+  detailElement.textContent = `${label}: ${value}`;
+  return detailElement;
+}
+
+function getDetails(data, type) {
+  if (type === "people") {
+    return [
+      { label: "Homeworld", value: data.homeworld },
+      { label: "Gender", value: data.gender },
+      { label: "Height", value: data.height },
+      { label: "Mass", value: data.mass },
+      { label: "Birth Year", value: data.birth_year },
+      { label: "Starships", value: data.starships.join(", ") },
+      { label: "Films", value: data.films.join(", ") },
+    ];
+  } else if (type === "planets") {
+    return [
+      { label: "Population", value: data.population },
+      { label: "Diameter", value: data.diameter },
+      { label: "Rotation Period", value: data.rotation_period },
+      { label: "Climate", value: data.climate },
+      { label: "Terrain", value: data.terrain },
+      { label: "Gravity", value: data.gravity },
+      { label: "Surface Water", value: data.surface_water },
+    ];
+  } else if (type === "starships") {
+    return [
+      { label: "Model", value: data.model },
+      { label: "Manufacturer", value: data.manufacturer },
+      { label: "Passengers", value: data.passengers },
+      { label: "Credits", value: data.cost_in_credits },
+      { label: "Length", value: data.length },
+      { label: "Starship Class", value: data.starship_class },
+      { label: "Max Speed", value: data.max_atmosphering_speed },
+    ];
+  }
+  return [];
 }
 
 function showPopup(item, type) {
@@ -91,46 +106,10 @@ function showPopup(item, type) {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      let details = [];
-
-      if (type === "people") {
-        details = [
-          { label: "Homeworld", value: data.homeworld },
-          { label: "Gender", value: data.gender },
-          { label: "Height", value: data.height },
-          { label: "Mass", value: data.mass },
-          { label: "Birth Year", value: data.birth_year },
-          { label: "Starships", value: data.starships.join(", ") },
-          { label: "Films", value: data.films.join(", ") },
-        ];
-      } else if (type === "planets") {
-        details = [
-          { label: "Population", value: data.population },
-          { label: "Diameter", value: data.diameter },
-          { label: "Rotation Period", value: data.rotation_period },
-          { label: "Climate", value: data.climate },
-          { label: "Terrain", value: data.terrain },
-          { label: "Gravity", value: data.gravity },
-          { label: "Surface Water", value: data.surface_water },
-        ];
-      } else if (type === "starships") {
-        details = [
-          { label: "Model", value: data.model },
-          { label: "Manufacturer", value: data.manufacturer },
-          { label: "Passengers", value: data.passengers },
-          { label: "Credits", value: data.cost_in_credits },
-          { label: "Length", value: data.length },
-          { label: "Starship Class", value: data.starship_class },
-          {
-            label: "Max Speed",
-            value: data.max_atmosphering_speed,
-          },
-        ];
-      }
+      const details = getDetails(data, type);
 
       details.forEach(({ label, value }) => {
-        const detailElement = document.createElement("h5");
-        detailElement.textContent = `${label}: ${value}`;
+        const detailElement = createDetailElement(label, value);
         popup.appendChild(detailElement);
       });
     });
@@ -152,26 +131,55 @@ function generateOverlay() {
   });
 }
 
+function responseApi(type) {
+  let itemUrl = `https://swapi.dev/api/${type}`;
+
+  return function () {
+    const loadBtn = document.querySelector(".load-btn");
+
+    fetch(itemUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        itemUrl = data.next;
+
+        if (data.next) {
+          loadBtn.classList.add("show");
+        } else {
+          loadBtn.classList.remove("show");
+        }
+        showItems(data.results, type);
+        window.scrollTo({
+          top: document.body.scrollHeight,
+          behavior: "smooth",
+        });
+      });
+  };
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   showNavigation();
   showList();
   generateOverlay();
 
   document.addEventListener("click", (event) => {
+    const selectedButton = document.querySelector(".selected");
+    let newResponseApi;
+
     if (
       event.target.tagName === "BUTTON" &&
       event.target.parentElement.tagName === "NAV"
     ) {
-      const selectedButton = document.querySelector("button.selected");
-
       if (selectedButton) {
         selectedButton.classList.remove("selected");
       }
-
       event.target.classList.add("selected");
-      responseApi(event.target.id);
+      newResponseApi = responseApi(event.target.id);
+      newResponseApi();
     } else if (event.target.classList.contains("load-btn")) {
-      responseApi();
+      if (selectedButton) {
+        newResponseApi = responseApi(selectedButton.id);
+        newResponseApi();
+      }
     }
   });
 });
